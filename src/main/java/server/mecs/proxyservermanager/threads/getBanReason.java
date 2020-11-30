2f4ae3fe848c95d2b85608eac25c9a853acd6f66
Chiharu-Hagihara <chiharuhagihara803@gmail.com>
2020-11-30 22:22:22 +0900

    getBanReason Runnable -> Callable

--- src/main/java/server/mecs/proxyservermanager/threads/getBanReason.java
+++ src/main/java/server/mecs/proxyservermanager/threads/getBanReason.java
@@ -5,37 +5,35 @@ import server.mecs.proxyservermanager.database.MySQLManager;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.concurrent.*;
 
-public class getBanReason extends Thread{
+public class getBanReason implements Callable<String> {
 
     ProxyServerManager plugin;
     String mcid;
-    public static String result = null;
 
     public getBanReason(ProxyServerManager plugin, String mcid){
         this.plugin = plugin;
         this.mcid = mcid;
-        result = null;
     }
 
-    public void run(){
+    public static String getBanReason(ProxyServerManager plugin, String mcid) throws InterruptedException, ExecutionException {
+        ExecutorService ex = Executors.newSingleThreadExecutor();
+        Future<String> futureresult = ex.submit(new getBanReason(plugin, mcid));
+        return futureresult.get();
+    }
+
+    @Override
+    public String call() throws Exception {
         MySQLManager mysql = new MySQLManager(plugin, "getBanReason");
         ResultSet rs = mysql.query("SELECT * FROM player_data WHERE mcid='" + mcid + "';");
         try {
-            result = rs.getString("ban_reason");
+            return rs.getString("ban_reason");
         } catch (SQLException e) {
             e.printStackTrace();
         }finally {
             mysql.close();
         }
-        result = null;
+        return null;
     }
-
-    public static String getBanReason(ProxyServerManager plugin, String mcid) throws InterruptedException {
-        getBanReason getBanReason = new getBanReason(plugin, mcid);
-        getBanReason.start();
-        getBanReason.join();
-        return result;
-    }
-
 }
