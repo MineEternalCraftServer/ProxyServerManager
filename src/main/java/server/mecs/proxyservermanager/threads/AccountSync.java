375f230782616a6ff3f954d42de593d171e009b7
Chiharu-Hagihara <chiharuhagihara803@gmail.com>
2020-11-30 16:01:09 +0900

    AccountSync 非同期化

--- src/main/java/server/mecs/proxyservermanager/threads/AccountSync.java
+++ src/main/java/server/mecs/proxyservermanager/threads/AccountSync.java
@@ -3,11 +3,27 @@ package server.mecs.proxyservermanager.threads;
 import server.mecs.proxyservermanager.ProxyServerManager;
 import server.mecs.proxyservermanager.database.MySQLManager;
 
-public class AccountSync{
+public class AccountSync extends Thread{
 
-    public static void AccountSync(ProxyServerManager plugin, String player, Long id){
+    ProxyServerManager plugin;
+    String player;
+    Long id;
+
+    public AccountSync(ProxyServerManager plugin, String player, Long id){
+        this.plugin = plugin;
+        this. player = player;
+        this.id = id;
+    }
+
+    public void run(){
         MySQLManager mysql = new MySQLManager(plugin, "AccountSync");
         mysql.execute("UPDATE player_data SET discord_link='" + id + "' WHERE mcid='" + player + "';");
         mysql.close();
+    }
+
+    public static void AccountSync(ProxyServerManager plugin, String player, Long id) throws InterruptedException {
+        AccountSync accountSync = new AccountSync(plugin, player, id);
+        accountSync.start();
+        accountSync.join();
     }
 }
