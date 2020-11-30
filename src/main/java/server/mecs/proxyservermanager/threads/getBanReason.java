f30faa11ed82d6c68805837fde5b0eacc18edd73
Chiharu-Hagihara <chiharuhagihara803@gmail.com>
2020-11-30 16:26:17 +0900

    getBanReason 非同期化

--- src/main/java/server/mecs/proxyservermanager/threads/getBanReason.java
+++ src/main/java/server/mecs/proxyservermanager/threads/getBanReason.java
@@ -6,19 +6,36 @@ import server.mecs.proxyservermanager.database.MySQLManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class getBanReason {
+public class getBanReason extends Thread{
 
-    public static String getBanReason(ProxyServerManager plugin, String mcid){
+    ProxyServerManager plugin;
+    String mcid;
+    public static String result = null;
+
+    public getBanReason(ProxyServerManager plugin, String mcid){
+        this.plugin = plugin;
+        this.mcid = mcid;
+        result = null;
+    }
+
+    public void run(){
         MySQLManager mysql = new MySQLManager(plugin, "getBanReason");
         ResultSet rs = mysql.query("SELECT * FROM player_data WHERE mcid='" + mcid + "';");
         try {
-            return rs.getString("ban_reason");
+            result = rs.getString("ban_reason");
         } catch (SQLException e) {
             e.printStackTrace();
         }finally {
             mysql.close();
         }
-        return null;
+        result = null;
+    }
+
+    public static String getBanReason(ProxyServerManager plugin, String mcid) throws InterruptedException {
+        getBanReason getBanReason = new getBanReason(plugin, mcid);
+        getBanReason.start();
+        getBanReason.join();
+        return result;
     }
 
 }
